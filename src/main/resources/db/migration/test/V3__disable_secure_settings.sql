5cfb74959bf54b76460df2819eb237eba4d85f67
privetEdik <wolcow88edyard@gmail.com>
2025-02-10 14:46:30 +0200

    optim refact reload in migrat

--- src/main/resources/db/migration/test/V3__disable_secure_settings.sql
+++ src/main/resources/db/migration/test/V3__disable_secure_settings.sql
@@ -2,4 +2,3 @@
 ALTER SYSTEM SET fsync = off;
 ALTER SYSTEM SET synchronous_commit = off;
 ALTER SYSTEM SET full_page_writes = off;
-SELECT pg_reload_conf();
--- src/main/resources/db/migration/test/V8__turn_secure_settings_back_on.sql
+++ src/main/resources/db/migration/test/V8__turn_secure_settings_back_on.sql
@@ -2,7 +2,6 @@
 ALTER SYSTEM SET fsync = on;
 ALTER SYSTEM SET synchronous_commit = on;
 ALTER SYSTEM SET full_page_writes = on;
-SELECT pg_reload_conf();
 
--- Оптимизируем БД после вставки
-VACUUM FULL ANALYZE;
+
+
